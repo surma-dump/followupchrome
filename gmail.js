1f0528b13d6cbb4cee900c59827e6baf9f31f1b0
Alexander Surma <surma@surmair.de>
2012-07-07 01:51:36 +0200

    Implement button click

--- gmail.js
+++ gmail.js
@@ -1,3 +1,14 @@
-var el = document.createElement('div');
-el.innerHTML = "HAI";
-document.getElementsByTagName('body')[0].appendChild(el);
+(function() {
+	var qry = document.location.search;
+	if(qry.indexOf('source=mailto') == -1 ||
+		qry.indexOf('@followupthen.com') == -1) {
+		return;
+	}
+	setTimeout(function lookForSendButton() {
+		var btn = document.getElementById(':pj');
+		if(btn == undefined || btn == null) {
+			return;
+		}
+		btn.click();
+	}, 1500);
+})();
