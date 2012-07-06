7399621e0d1bf67689fb327ea53d0db5681974f9
Alexander Surma <surma@surmair.de>
2012-07-06 13:48:05 +0200

    Use SCSS and design buttons

---
+++ config.rb
@@ -0,0 +1,8 @@
+# Require any additional compass plugins here.
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+output_style = :compressed
+
